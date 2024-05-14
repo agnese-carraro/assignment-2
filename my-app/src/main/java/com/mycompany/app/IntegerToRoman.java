fe1c79e618f176a43ba01529ef61f6caeac9f4a6
agnese-carraro <161816812+agnese-carraro@users.noreply.github.com>
2024-05-14 12:54:26 +0200

    Add files via upload

---
+++ my-app/src/main/java/com/mycompany/app/IntegerToRoman.java
@@ -0,0 +1,46 @@
+
+package com.mycompany.app;
+
+public class IntegerToRoman {
+    public static String conv(int number) {
+        String result = "";
+        String[] c1 = {"M"};
+        // da aggiungere al terzo passo.
+        String[] c2 = {"C","CC","CCC","CD","D", "DC", "DCC", "DCCC", "CM"};
+        // da aggiungere al secondo passo.
+        String[] c3 = {"X","XX","XXX","XL","L", "LX", "LXX", "LXXX", "XC"};
+        // da tenere dall'inizio
+        String[] c4 = {"I","II","III","IV","V", "VI", "VII", "VIII", "IX"};
+
+        int n = number /1000;
+        if (n != 0) {
+            result = result.concat(c1[n-1]);
+            number  = number - n*1000;
+        }
+
+
+        // da aggiungere al terzo passo.
+        n = number /100;
+        if (n != 0) {
+            result = result.concat(c2[n-1]);
+            number  = number - n*100;
+        }
+
+
+        // da aggiungere al seecondo passo.
+        n = number /10;
+        if (n != 0) {
+            result = result.concat(c3[n-1]);
+            number  = number - n*10;
+        }
+
+        // da tenere dall'inizio
+        if (number > 0 && number < 10) {
+            result = result.concat(c4[number-1]);
+        }
+
+        if (!result.isEmpty()) {return result;}
+        else {return null;}
+    }
+
+}
---
+++ my-app/src/test/java/com/mycompany/app/IntegerToRomanTest.java
@@ -0,0 +1,42 @@
+////////////////////////////////////////////////////////////////////
+// [NOME1] [COGNOME1] [MATRICOLA1]
+// [NOME2] [COGNOME2] [MATRICOLA2]
+////////////////////////////////////////////////////////////////////
+
+package com.mycompany.app;
+
+import org.junit.Test;
+
+import static com.mycompany.app.IntegerToRoman.conv;
+import static org.junit.Assert.assertEquals;
+
+public class IntegerToRomanTest {
+    @Test
+    public void test() {
+        String[] c1 = {"M"};
+        String[] c2 = {"C","CC","CCC","CD","D", "DC", "DCC", "DCCC", "CM"};
+        String[] c3 = {"X","XX","XXX","XL","L", "LX", "LXX", "LXXX", "XC"};
+        String[] c4 = {"I","II","III","IV","V", "VI", "VII", "VIII", "IX"};
+        String[][] c = {c1, c2, c3, c4};
+        //int j = 3;
+        for (int i = 1; i <= 9; i++) {
+            String result = conv(i);
+            assertEquals(c4[i-1], result);
+            //j--;
+        }
+    }
+
+    @Test
+    public void test2() {
+        int i = 0;
+        String result = conv(i);
+        assertEquals(null, result);
+    }
+
+    @Test
+    public void test3() {
+        int i = 19;
+        String result = conv(i);
+        assertEquals(null, result);
+    }
+}
